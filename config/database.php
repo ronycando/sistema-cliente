43737b065c94032e2871cc60e7d8584655e0782e
Rony Israel Cando Jacome <rony.israelcj@gmail.com>
2026-09-08 07:42:49 -0500

    Configuración de conexión con MySQL

--- config/database.php
+++ config/database.php
@@ -1,3 +1,42 @@
 <?php
-// incio
+
+class Conectar
+{
+    protected $dbh;
+
+    protected function conexion()
+    {
+        try {
+
+            $conectar = $this->dbh = new PDO(
+                "mysql:host=localhost;dbname=integradora",
+                "root",
+                "cynerisai"
+            );
+
+            $conectar->setAttribute(
+                PDO::ATTR_ERRMODE,
+                PDO::ERRMODE_EXCEPTION
+            );
+
+            $conectar->setAttribute(
+                PDO::ATTR_DEFAULT_FETCH_MODE,
+                PDO::FETCH_ASSOC
+            );
+
+            return $conectar;
+
+        } catch (PDOException $e) {
+
+            die("Error de conexión a la base de datos: " . $e->getMessage());
+
+        }
+    }
+
+    public function set_names()
+    {
+        return $this->dbh->query("SET NAMES 'utf8mb4'");
+    }
+}
+
 ?>
